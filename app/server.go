504ce59a71e2b7cac7c6f19322e5e547760841ac
LOVESH1808 <loveshdutta1808@gmail.com>
2024-03-27 07:06:08 +0530

    Testing

--- app/server.go
+++ app/server.go
@@ -27,15 +27,30 @@ func main() {
 	}
 
 	defer conn.Close()
-	request := strings.Split(strings.Split(string(buff), "\n")[0], " ")
+	headers := make(map[string]string)
+	request := strings.Split(string(buff), "\n")
+	head := strings.Split(request[0], " ")
+	for _, val := range request[1:] {
+		if !strings.Contains(val, ":") {
+			break
+		}
+		print(strings.Split(val, " ")[0])
+		headers[strings.Split(val, " ")[0]] = strings.Trim(strings.Split(val, " ")[1], "\r")
+
+	}
 	var response []byte
-	if len(request[1]) > 5 && request[1][:6] == "/echo/" {
-		str := request[1][6:]
+	if len(head[1]) > 5 && head[1][:6] == "/echo/" {
+		str := head[1][6:]
 		length := len(str)
 		rsp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s\r\n", length, str)
 		response = []byte(rsp)
+	} else if head[1] == "/user-agent" {
+		str := headers["User-Agent:"]
+		length := len(str)
+		rsp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s\r\n", length, str)
 
-	} else if request[1] == "/" {
+		response = []byte(rsp)
+	} else if head[1] == "/" {
 		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
 	} else {
 		response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
