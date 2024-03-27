0169c975ce132dc16f595dae268e5cccaa007eb9
LOVESH1808 <loveshdutta1808@gmail.com>
2024-03-27 06:59:56 +0530

    testing

--- app/server.go
+++ app/server.go
@@ -24,4 +24,10 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
